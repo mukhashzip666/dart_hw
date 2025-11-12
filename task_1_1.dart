f7c569dfc505600e99c67797253506db6a7caf94
mukhashzip666 <mukhammed8021@icloud.com>
2025-11-12 11:47:23 +0500

    2ші уй жумысы

---
+++ task_1_1.dart
@@ -0,0 +1,8 @@
+void main() {
+  String name = "Муха";
+  int age = 17;
+  String city = "Кзо";
+  bool isStudent = true;
+
+  print("Сәлем! Менің атым $name, мен $age жастамын. Мен $city қаласында тұрамын. Мен студентпін: $isStudent");
+}
